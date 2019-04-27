770c3ad4b843212938711592182ca4e847d6c28e
Gabriel Trainning <gabrielcasanova328@gmail.com>
2019-04-27 12:26:16 -0300

    Hello World
    version name 0.2
    
    .java
    Vincular componentes
    Evento para botão
    usando set e get String
    
    Signed-off-by: Gabriel Trainning <gabrielcasanova328@gmail.com>

--- HelloWord/app/src/main/java/casanova/gabriel/helloworldtrainnig/MainActivity.java
+++ HelloWord/app/src/main/java/casanova/gabriel/helloworldtrainnig/MainActivity.java
@@ -2,6 +2,9 @@ package casanova.gabriel.helloworldtrainnig;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Vincular os componentes
+        final   TextView    txv_result      = findViewById(R.id.txv_result  );
+                Button      btn_run         = findViewById(R.id.btn_run     );
+        //Fim Vincular os componentes
+
+        //Evento do botão
+        btn_run.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (txv_result.getText().toString().equals("Hello World!")) {
+                    txv_result.setText(getString(R.string.text_ola));
+                } else {
+                    txv_result.setText(getString(R.string.text_hello));
+                }
+            }
+        });
+        //Fim Evento do botão
     }
 }
