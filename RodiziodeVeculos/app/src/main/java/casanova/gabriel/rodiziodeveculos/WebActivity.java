0806a72b8f7cfe520f6abe21eea3a0d804c98146
Gabriel Trainning <gabrielcasanova328@gmail.com>
2019-05-04 09:50:26 -0300

    Rodizio de Veículos
    WebView implementado
    Internet permission
    
    Signed-off-by: Gabriel Trainning <gabrielcasanova328@gmail.com>

--- RodiziodeVeculos/app/src/main/java/casanova/gabriel/rodiziodeveculos/WebActivity.java
+++ RodiziodeVeculos/app/src/main/java/casanova/gabriel/rodiziodeveculos/WebActivity.java
@@ -2,6 +2,7 @@ package casanova.gabriel.rodiziodeveculos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class WebActivity extends AppCompatActivity {
 
@@ -9,5 +10,15 @@ public class WebActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web);
+
+
+        WebView webView = findViewById(R.id.wbv_mapa);
+
+
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        webView.loadUrl("https://www.google.com/maps/d/viewer?mid=1Xg6ACUNSiuS0IOr7p7t9c6IvLx4&msa=0&ll=-23.56894967405638%2C-46.660604999999975&z=12");
+
+
     }
 }
