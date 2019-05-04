21c96d62d47462c7e3f5ea41dbe664f00dd5c3c7
Gabriel Trainning <gabrielcasanova328@gmail.com>
2019-05-04 12:42:17 -0300

    Verificar CPF
    vibrar implementado
    
    Signed-off-by: Gabriel Trainning <gabrielcasanova328@gmail.com>

--- CalculoCPF/app/src/main/java/casanova/gabriel/verificarcpf/MainActivity.java
+++ CalculoCPF/app/src/main/java/casanova/gabriel/verificarcpf/MainActivity.java
@@ -1,5 +1,6 @@
 package casanova.gabriel.verificarcpf;
 
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     //Declarando componentes
     private EditText  edt_cpf;
     private ImageView img_cpf;
+    private Vibrator vibrator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,19 +22,24 @@ public class MainActivity extends AppCompatActivity {
 
         edt_cpf = findViewById(R.id.edt_cpf);
         img_cpf = findViewById(R.id.img_cpf);
+
+        //Istancinando Serviço
+        vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
     }
 
     public void onVerificar(View view) {
         String cpf = edt_cpf.getEditableText().toString().trim();
 
-        if (cpf.length() != 11) {
+        if (cpf.length() < 11) {
             Toast.makeText(this,"Digite um CPF com 11 números",Toast.LENGTH_LONG).show();
+            vibrator.vibrate(500);
         } else {
             Calculos calculos = new Calculos();
             if (calculos.verificar(cpf)) {
                 img_cpf.setImageResource (R.drawable.ic_thumb_up);
             } else {
                 img_cpf.setImageResource (R.drawable.ic_thumb_down);
+                vibrator.vibrate(500);
             }
         }
     }
